9f6a7cdd76cbd69a54a3c266eae894292e637dff
James Lippitt <james.lippitt@triptease.com>
2019-02-24 05:33:29 +0000

    Implement other basic flag instructions

--- src/cpu/operation/flags.ts
+++ src/cpu/operation/flags.ts
@@ -2,6 +2,42 @@ import { debug } from 'log';
 
 import State from '../state';
 
+export function clc({ flags, clock }: State) {
+  debug('CLC');
+  flags.carry = false;
+  clock.tick(2);
+}
+
+export function cld({ flags, clock }: State) {
+  debug('CLD');
+  flags.decimal = false;
+  clock.tick(2);
+}
+
+export function cli({ flags, clock }: State) {
+  debug('CLI');
+  flags.interrupt = false;
+  clock.tick(2);
+}
+
+export function clv({ flags, clock }: State) {
+  debug('CLV');
+  flags.overflow = false;
+  clock.tick(2);
+}
+
+export function sec({ flags, clock }: State) {
+  debug('SEC');
+  flags.carry = true;
+  clock.tick(2);
+}
+
+export function sed({ flags, clock }: State) {
+  debug('SED');
+  flags.decimal = true;
+  clock.tick(2);
+}
+
 export function sei({ flags, clock }: State) {
   debug('SEI');
   flags.interrupt = true;
--- src/cpu/operation/index.ts
+++ src/cpu/operation/index.ts
@@ -1,6 +1,6 @@
 import State from '../state';
 
-import { sei } from './flags';
+import { clc, cld, cli, clv, sec, sed, sei } from './flags';
 
 type Operation = (state: State) => void;
 
@@ -17,7 +17,7 @@ export const opMap: Operation[] = [
   // 0x10
   xxx, xxx, xxx, xxx,
   xxx, xxx, xxx, xxx,
-  xxx, xxx, xxx, xxx,
+  clc, xxx, xxx, xxx,
   xxx, xxx, xxx, xxx,
   // 0x20
   xxx, xxx, xxx, xxx,
@@ -27,7 +27,7 @@ export const opMap: Operation[] = [
   // 0x30
   xxx, xxx, xxx, xxx,
   xxx, xxx, xxx, xxx,
-  xxx, xxx, xxx, xxx,
+  sec, xxx, xxx, xxx,
   xxx, xxx, xxx, xxx,
   // 0x40
   xxx, xxx, xxx, xxx,
@@ -37,7 +37,7 @@ export const opMap: Operation[] = [
   // 0x50
   xxx, xxx, xxx, xxx,
   xxx, xxx, xxx, xxx,
-  xxx, xxx, xxx, xxx,
+  cli, xxx, xxx, xxx,
   xxx, xxx, xxx, xxx,
   // 0x60
   xxx, xxx, xxx, xxx,
@@ -67,7 +67,7 @@ export const opMap: Operation[] = [
   // 0xB0
   xxx, xxx, xxx, xxx,
   xxx, xxx, xxx, xxx,
-  xxx, xxx, xxx, xxx,
+  clv, xxx, xxx, xxx,
   xxx, xxx, xxx, xxx,
   // 0xC0
   xxx, xxx, xxx, xxx,
@@ -77,7 +77,7 @@ export const opMap: Operation[] = [
   // 0xD0
   xxx, xxx, xxx, xxx,
   xxx, xxx, xxx, xxx,
-  xxx, xxx, xxx, xxx,
+  cld, xxx, xxx, xxx,
   xxx, xxx, xxx, xxx,
   // 0xE0
   xxx, xxx, xxx, xxx,
@@ -87,6 +87,6 @@ export const opMap: Operation[] = [
   // 0xF0
   xxx, xxx, xxx, xxx,
   xxx, xxx, xxx, xxx,
-  xxx, xxx, xxx, xxx,
+  sed, xxx, xxx, xxx,
   xxx, xxx, xxx, xxx,
 ];
