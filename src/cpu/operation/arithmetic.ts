7e9052913859d222e5594cde7b7d7a1c26355a7b
James Lippitt <james.lippitt@triptease.com>
2019-03-16 10:11:05 +0000

    Implement subtraction

---
+++ src/cpu/operation/arithmetic.ts
@@ -0,0 +1,28 @@
+import { debug, toHex } from 'log';
+
+import State from '../state';
+import AddressMode from './addressMode';
+
+function subtract(state: State, rhs: number) {
+  const { flags, regs, clock } = state;
+  const lhs = regs.a;
+  regs.a = (lhs - rhs - (flags.carry ? 0 : 1)) & 0xff;
+  flags.setZeroAndNegative(regs.a);
+  flags.carry = regs.a > lhs;
+  flags.overflow = ((lhs ^ regs.a) & (rhs ^ regs.a) & 0x80) !== 0;
+  clock.tick(2);
+}
+
+export const sbc = (addressMode: AddressMode) => (state: State) => {
+  const { mmu } = state;
+  debug(`SBC ${addressMode}`);
+  const address = addressMode.lookup(state, true);
+  const value = mmu.getByte(address);
+  subtract(state, value);
+};
+
+export function sbcImmediate(state: State) {
+  const value = state.nextByte();
+  debug(`SBC ${toHex(value, 2)}`);
+  subtract(state, value);
+}
--- src/cpu/operation/bitwise.ts
+++ src/cpu/operation/bitwise.ts
@@ -3,6 +3,15 @@ import { debug, toHex } from 'log';
 import State from '../state';
 import AddressMode from './addressMode';
 
+export const and = (addressMode: AddressMode) => (state: State) => {
+  const { regs, flags, mmu, clock } = state;
+  debug(`AND ${addressMode}`);
+  const address = addressMode.lookup(state, true);
+  regs.a &= mmu.getByte(address);
+  flags.setZeroAndNegative(regs.a);
+  clock.tick(2);
+};
+
 export function andImmediate(state: State) {
   const { regs, flags, clock } = state;
   const value = state.nextByte();
@@ -15,7 +24,7 @@ export function andImmediate(state: State) {
 export const bit = (addressMode: AddressMode) => (state: State) => {
   const { regs, mmu, flags, clock } = state;
   debug(`BIT ${addressMode}`);
-  const address = addressMode.lookup(state);
+  const address = addressMode.lookup(state, false);
   const value = mmu.getByte(address);
   flags.zero = (regs.a & value) === 0;
   flags.overflow = (value & 0x40) !== 0;
@@ -26,7 +35,7 @@ export const bit = (addressMode: AddressMode) => (state: State) => {
 export const ora = (addressMode: AddressMode) => (state: State) => {
   const { regs, flags, mmu, clock } = state;
   debug(`ORA ${addressMode}`);
-  const address = addressMode.lookup(state);
+  const address = addressMode.lookup(state, true);
   regs.a |= mmu.getByte(address);
   flags.setZeroAndNegative(regs.a);
   clock.tick(2);
