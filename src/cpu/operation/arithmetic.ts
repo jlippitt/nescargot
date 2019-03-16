3a8e30ee4665e35936ae022a0bc33741fc78e713
James Lippitt <james.lippitt@triptease.com>
2019-03-16 10:12:43 +0000

    Fix carry flag result in SBC

--- src/cpu/operation/arithmetic.ts
+++ src/cpu/operation/arithmetic.ts
@@ -8,7 +8,7 @@ function subtract(state: State, rhs: number) {
   const lhs = regs.a;
   regs.a = (lhs - rhs - (flags.carry ? 0 : 1)) & 0xff;
   flags.setZeroAndNegative(regs.a);
-  flags.carry = regs.a > lhs;
+  flags.carry = regs.a <= lhs;
   flags.overflow = ((lhs ^ regs.a) & (rhs ^ regs.a) & 0x80) !== 0;
   clock.tick(2);
 }
