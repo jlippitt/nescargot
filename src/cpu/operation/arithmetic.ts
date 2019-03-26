30d90805ce9c0aed9dd38b12573b27c0a4338661
James Lippitt <james.lippitt@triptease.com>
2019-03-26 15:29:22 +0000

    Fix ADC so it passes tests

--- src/cpu/operation/arithmetic.ts
+++ src/cpu/operation/arithmetic.ts
@@ -6,10 +6,11 @@ import State from '../state';
 function add(state: State, rhs: number) {
   const { flags, regs, clock } = state;
   const lhs = regs.a;
-  regs.a = (lhs + rhs + (flags.carry ? 1 : 0)) & 0xff;
+  const result = lhs + rhs + (flags.carry ? 1 : 0);
+  regs.a = result & 0xff;
   flags.setZeroAndNegative(regs.a);
-  flags.carry = regs.a < lhs;
-  flags.overflow = (~(lhs ^ regs.a) & (rhs ^ regs.a) & 0x80) !== 0;
+  flags.carry = result > 0xff;
+  flags.overflow = (~(lhs ^ rhs) & (rhs ^ regs.a) & 0x80) !== 0;
   clock.tick(2);
 }
 
