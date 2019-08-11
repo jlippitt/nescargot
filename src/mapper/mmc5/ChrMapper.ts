c0d6e1f1cd847bb9a90707a76a76119b1fa0da82
James Lippitt <james.lippitt@triptease.com>
2019-08-11 12:21:20 +0100

    Extract MMC5 CHR mapper code into its own class

---
+++ src/mapper/mmc5/ChrMapper.ts
@@ -0,0 +1,112 @@
+import { debug } from 'log';
+import Pattern from 'ppu/Pattern';
+
+const BANK_SIZE = 64;
+
+export default class ChrMapper {
+  private chr: Pattern[];
+  private mode: number = 3;
+  private register: number[];
+  private offset: number[];
+
+  constructor(chr: Pattern[]) {
+    this.chr = chr;
+    this.register = Array(12).fill(0);
+    this.offset = Array(16).fill(0);
+    this.updateBanks();
+  }
+
+  public getPattern(index: number): Pattern {
+    // TODO: Higher pattern tables?
+    return this.chr[this.offset[(index & 0x01c0) >> 6] | (index & 0x003f)];
+  }
+
+  public setMode(value: number): void {
+    this.mode = value & 0x03;
+    debug(`CHR Mode ${this.mode}`);
+    this.updateBanks();
+  }
+
+  public setRegister(index: number, value: number): void {
+    this.register[index] = value;
+    this.updateBanks();
+  }
+
+  private updateBanks(): void {
+    switch (this.mode) {
+      case 0:
+        // Lower banks
+        for (let i = 0; i < 8; ++i) {
+          this.offset[i] = this.getOffset((this.register[7] << 3) + i);
+        }
+
+        // Upper banks
+        for (let i = 0; i < 8; ++i) {
+          this.offset[8 + i] = this.getOffset((this.register[11] << 3) + i);
+        }
+        break;
+
+      case 1:
+        // Lower banks
+        for (let i = 0; i < 2; ++i) {
+          const index = i * 4;
+          const value = this.register[index + 1] << 2;
+
+          for (let j = 0; j < 4; ++j) {
+            this.offset[index + j] = this.getOffset(value + j);
+          }
+        }
+
+        // Upper banks
+        for (let i = 0; i < 2; ++i) {
+          const index = 8 + i * 4;
+          const value = this.register[11] << 2;
+
+          for (let j = 0; j < 4; ++j) {
+            this.offset[index + j] = this.getOffset(value + j);
+          }
+        }
+        break;
+
+      case 2:
+        // Lower banks
+        for (let i = 0; i < 4; ++i) {
+          const index = i * 2;
+          const value = this.register[index + 1] << 1;
+          this.offset[index] = this.getOffset(value);
+          this.offset[index + 1] = this.getOffset(value + 1);
+        }
+
+        // Upper banks
+        for (let i = 0; i < 4; ++i) {
+          const index = 8 + i * 2;
+          const value = this.register[9 + 2 * (i % 2)] << 1;
+          this.offset[index] = this.getOffset(value);
+          this.offset[index + 1] = this.getOffset(value + 1);
+        }
+        break;
+
+      case 3:
+        // Lower banks
+        for (let i = 0; i < 8; ++i) {
+          this.offset[i] = this.getOffset(this.register[i]);
+        }
+
+        // Upper banks
+        for (let i = 0; i < 8; ++i) {
+          this.offset[8 + i] = this.getOffset(this.register[8 + (i % 4)]);
+        }
+        break;
+
+      default:
+        throw new Error('Should not happen');
+    }
+
+    for (let i = 0; i < 16; ++i) {
+      debug(`CHR ${i} = ${this.offset[i]}`);
+    }
+  }
+
+  private getOffset = (value: number): number =>
+    (value * BANK_SIZE) % this.chr.length
+}
--- src/mapper/mmc5/MMC5.ts
+++ src/mapper/mmc5/MMC5.ts
@@ -5,24 +5,19 @@ import Pattern from 'ppu/Pattern';
 import AbstractMapper from '../AbstractMapper';
 import { MapperOptions } from '../Mapper';
 
+import ChrMapper from './ChrMapper';
 import PrgMapper from './PrgMapper';
-
-const CHR_BANK_SIZE = 64;
 
 export default class MMC5 extends AbstractMapper {
   private prgMapper: PrgMapper;
-  private chrMode: number = 3;
-  private chrRegister: number[];
-  private chrOffset: number[];
+  private chrMapper: ChrMapper;
   private nameTableOffset: number[];
 
   constructor(options: MapperOptions) {
     super(options);
     this.prgMapper = new PrgMapper({ rom: this.prgRom, ram: this.prgRam });
-    this.chrRegister = Array(12).fill(0);
-    this.chrOffset = Array(16).fill(0);
+    this.chrMapper = new ChrMapper(this.chr);
     this.nameTableOffset = Array(4).fill(0);
-    this.updateChrBanks();
   }
 
   public getPrgByte(offset: number): number {
@@ -45,8 +40,7 @@ export default class MMC5 extends AbstractMapper {
   }
 
   public getPattern(index: number): Pattern {
-    // TODO: Higher pattern tables?
-    return this.chr[this.chrOffset[(index & 0x01c0) >> 6] | (index & 0x003f)];
+    return this.chrMapper.getPattern(index);
   }
 
   public getNameTable(index: number): NameTable {
@@ -67,8 +61,7 @@ export default class MMC5 extends AbstractMapper {
         break;
 
       case 0x5101:
-        this.chrMode = value & 0x03;
-        debug(`CHR Mode ${this.chrMode}`);
+        this.chrMapper.setMode(value);
         break;
 
       case 0x5104:
@@ -111,8 +104,7 @@ export default class MMC5 extends AbstractMapper {
       case 0x5129:
       case 0x512a:
       case 0x512b:
-        this.chrRegister[offset - 0x5120] = value;
-        this.updateChrBanks();
+        this.chrMapper.setRegister(offset - 0x5120, value);
         break;
 
       case 0x5200:
@@ -132,86 +124,4 @@ export default class MMC5 extends AbstractMapper {
         );
     }
   }
-
-  private updateChrBanks(): void {
-    switch (this.chrMode) {
-      case 0:
-        // Lower banks
-        for (let i = 0; i < 8; ++i) {
-          this.chrOffset[i] = this.getChrOffset((this.chrRegister[7] << 3) + i);
-        }
-
-        // Upper banks
-        for (let i = 0; i < 8; ++i) {
-          this.chrOffset[8 + i] = this.getChrOffset(
-            (this.chrRegister[11] << 3) + i,
-          );
-        }
-        break;
-
-      case 1:
-        // Lower banks
-        for (let i = 0; i < 2; ++i) {
-          const index = i * 4;
-          const value = this.chrRegister[index + 1] << 2;
-
-          for (let j = 0; j < 4; ++j) {
-            this.chrOffset[index + j] = this.getChrOffset(value + j);
-          }
-        }
-
-        // Upper banks
-        for (let i = 0; i < 2; ++i) {
-          const index = 8 + i * 4;
-          const value = this.chrRegister[11] << 2;
-
-          for (let j = 0; j < 4; ++j) {
-            this.chrOffset[index + j] = this.getChrOffset(value + j);
-          }
-        }
-        break;
-
-      case 2:
-        // Lower banks
-        for (let i = 0; i < 4; ++i) {
-          const index = i * 2;
-          const value = this.chrRegister[index + 1] << 1;
-          this.chrOffset[index] = this.getChrOffset(value);
-          this.chrOffset[index + 1] = this.getChrOffset(value + 1);
-        }
-
-        // Upper banks
-        for (let i = 0; i < 4; ++i) {
-          const index = 8 + i * 2;
-          const value = this.chrRegister[9 + 2 * (i % 2)] << 1;
-          this.chrOffset[index] = this.getChrOffset(value);
-          this.chrOffset[index + 1] = this.getChrOffset(value + 1);
-        }
-        break;
-
-      case 3:
-        // Lower banks
-        for (let i = 0; i < 8; ++i) {
-          this.chrOffset[i] = this.getChrOffset(this.chrRegister[i]);
-        }
-
-        // Upper banks
-        for (let i = 0; i < 8; ++i) {
-          this.chrOffset[8 + i] = this.getChrOffset(
-            this.chrRegister[8 + (i % 4)],
-          );
-        }
-        break;
-
-      default:
-        throw new Error('Should not happen');
-    }
-
-    for (let i = 0; i < 16; ++i) {
-      debug(`CHR ${i} = ${this.chrOffset[i]}`);
-    }
-  }
-
-  private getChrOffset = (value: number): number =>
-    (value * CHR_BANK_SIZE) % this.chr.length
 }
