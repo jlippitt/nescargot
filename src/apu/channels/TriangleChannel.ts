b8d2759603fd6df1869250d58fd4ae85c2b4809c
James Lippitt <james.lippitt@gmail.com>
2019-08-30 08:50:07 +0100

    Only set linear counter reload when 00B is written to (not 008)

--- src/apu/channels/TriangleChannel.ts
+++ src/apu/channels/TriangleChannel.ts
@@ -8,39 +8,10 @@ import Sequencer, { Sequence } from './components/Sequencer';
 
 const TIMER_PERIOD_MULTIPLIER = 1;
 
+// prettier-ignore
 const TRIANGLE_SEQUENCE = [
-  15,
-  14,
-  13,
-  12,
-  11,
-  10,
-  9,
-  8,
-  7,
-  6,
-  5,
-  4,
-  3,
-  2,
-  1,
-  0,
-  0,
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  10,
-  11,
-  12,
-  13,
-  14,
-  15,
+  15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
 ];
 
 export default class TriangleChannel {
@@ -63,7 +34,6 @@ export default class TriangleChannel {
       case 0:
         this.lengthCounter.setHalted((value & 0x80) !== 0);
         this.linearCounter.setByte(value);
-        this.linearCounter.setReload();
         break;
       case 1:
         // Unused
